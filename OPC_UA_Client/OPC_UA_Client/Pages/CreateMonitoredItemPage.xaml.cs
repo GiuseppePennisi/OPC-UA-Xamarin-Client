e663731c023646040dc8d1a063b2158b2afb016b
GiuseppePennisi <giuseppe.pennisi11@gmail.com>
2018-06-21 12:55:33 +0200

    Added control on entries of created Monitored Item Page.

--- OPC_UA_Client/OPC_UA_Client/Pages/CreateMonitoredItemPage.xaml.cs
+++ OPC_UA_Client/OPC_UA_Client/Pages/CreateMonitoredItemPage.xaml.cs
@@ -34,23 +34,94 @@ namespace OPC_UA_Client.Pages
 
         private async void OnCreateMonitoredItem(object sender, EventArgs e)
         {
-            int typeID = TypeNodeIdPicker.SelectedIndex;
-            ushort namespaceIndex = Convert.ToUInt16(NodeNamespace.Text);
-            string identifierNode = NodeID.Text;
-            int samplingInterval = Convert.ToInt32(RequestedSamplingInterval.Text);
-            bool discardOldest = DiscardOldest.IsToggled;
-            uint queueSize = Convert.ToUInt32(QueueSize.Text);
-            int monitoringMode = MonitoringModePicker.SelectedIndex;
-            int filterTrigger = TriggerPicker.SelectedIndex;
-            uint deadbandType = Convert.ToUInt32(DeadbandTypePicker.SelectedIndex);
-            double deadbandValue = Convert.ToDouble(DeadbandValue.Text);
-            client.CreateMonitoredItem(subscriptionId, typeID, namespaceIndex, identifierNode, samplingInterval,discardOldest, queueSize, monitoringMode, filterTrigger, deadbandType, deadbandValue);
+            int typeID;
+            ushort namespaceIndex;
+            string identifierNode;
+            int samplingInterval;
+            bool discardOldest;
+            uint queueSize;
+            int monitoringMode;
+            int filterTrigger;
+            uint deadbandType;
+            double deadbandValue;
 
-            await DisplayAlert("Info", "Monitored Item Created Successfully", "Ok");
+            try
+            {
+                typeID = TypeNodeIdPicker.SelectedIndex;
+                if (string.IsNullOrEmpty(NodeID.Text))
+                    throw new EmptyEntryException("Empty Node ID Entry!");
+                if (typeID == 0)
+                {
+                    try
+                    {
+                        identifierNode = NodeID.Text;
+                    }
+                    catch (FormatException p)
+                    {
+                        throw new FormatException("Node ID Format not valid!", p);
+                    }
+                }
+                else { 
+                identifierNode = NodeID.Text;
+                }
+                if (string.IsNullOrEmpty(NodeNamespace.Text))
+                    throw new EmptyEntryException("Empty Node Namespace Entry!");
+                try { 
+                namespaceIndex = Convert.ToUInt16(NodeNamespace.Text);
+                }
+                catch(FormatException p)
+                {
+                    throw new FormatException("Node Namespace not valid!", p);
+                }
 
-            ContentPage detailSubPage = new DetailSubscriptionPage(client,subscriptionId);
-            detailSubPage.Title = "OPC Subscription Details";
-            await Navigation.PushAsync(detailSubPage);
+                if (string.IsNullOrEmpty(RequestedSamplingInterval.Text))
+                    throw new EmptyEntryException("Requested Sampling Interval Empty!");
+                try { 
+                samplingInterval = Convert.ToInt32(RequestedSamplingInterval.Text);
+                }
+                catch(FormatException p){
+                    throw new FormatException("Requested Sampling Interval format not valid!", p);
+                }
+                  discardOldest = DiscardOldest.IsToggled;
+                if (string.IsNullOrEmpty(QueueSize.Text))
+                    throw new EmptyEntryException("Empty Queue Size Entry!");
+                try
+                {
+                    queueSize = Convert.ToUInt32(QueueSize.Text);
+                }
+                catch(FormatException p)
+                {
+                    throw new FormatException("Queue Size format non valid!", p);
+                }
+                monitoringMode = MonitoringModePicker.SelectedIndex;
+                filterTrigger = TriggerPicker.SelectedIndex;
+                
+                deadbandType = Convert.ToUInt32(DeadbandTypePicker.SelectedIndex);
+                if (string.IsNullOrEmpty(DeadbandValue.Text))
+                    throw new EmptyEntryException("Empty Deadband Value Entry!");
+                try { 
+                deadbandValue = Convert.ToDouble(DeadbandValue.Text);
+                }
+                catch(FormatException p)
+                {
+                    throw new FormatException("Deadband Value Format is not valid!", p);
+                }
+                client.CreateMonitoredItem(subscriptionId, typeID, namespaceIndex, identifierNode, samplingInterval, discardOldest, queueSize, monitoringMode, filterTrigger, deadbandType, deadbandValue);
+
+                await DisplayAlert("Info", "Monitored Item Created Successfully", "Ok");
+
+                ContentPage detailSubPage = new DetailSubscriptionPage(client, subscriptionId);
+                detailSubPage.Title = "OPC Subscription Details";
+                await Navigation.PushAsync(detailSubPage);
+            }
+            catch(FormatException p)
+            {
+                await DisplayAlert("Error", p.Message, "Ok");
+            }
+            catch (EmptyEntryException p)
+            {
+                await DisplayAlert("Error", p.Message, "Ok");
+            }
         }
 
         protected override bool OnBackButtonPressed()
